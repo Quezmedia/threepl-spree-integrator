6825b402cb8278a3062f81d9b065f541c47d15c2
Filipe Chagas <filipe@ochagas.com>
2013-10-15 17:13:54 -0300

    Create order calls are now aorking

--- lib/nutracoapi/create_order.rb
+++ lib/nutracoapi/create_order.rb
@@ -6,39 +6,55 @@ class Nutracoapi::CreateOrder < Nutracoapi::Base
                         [prepare_order_body(options)]
                        }
     })
-    pp body
-    puts prepare_operation("CreateOrders", {}, body)
     call("CreateOrders", {}, body)
   end
 
   private
   def prepare_order_body(options = {})
     reference_num = options.fetch(:reference_num)
+    company_name  = options.fetch(:company_name)
+    address1      = options.fetch(:address1)
+    city          = options.fetch(:city)
+    state         = options.fetch(:state)
+    zip           = options.fetch(:zip)
+    country       = options.fetch(:country)
+    carrier       = options.fetch(:carrier)
+    mode          = options.fetch(:mode)
+    billing_code  = options.fetch(:billing_code)
+    account       = options.fetch(:account)
+    notes         = options.fetch(:notes)
+    products      = options.fetch(:products)
     {
       :TransInfo => {:ReferenceNum => reference_num},
       :ShipTo => {
-        :CompanyName => "Test Company Name",
+        :CompanyName => company_name,
         :Address => {
-          :Address1 => "Blablabla",
-          :City => "Los Angeles",
-          :State => "CA",
-          :Zip => "90010",
-          :Country => "US"
+          :Address1 => address1,
+          :City => city,
+          :State => state,
+          :Zip => zip,
+          :Country => country
         },
       },
       :ShippingInstructions => {
-        :Carrier => "UPS",
-        :Mode => "Ground",
-        :BillingCode => "FreightCollect",
-        :Account => "12345675"
+        :Carrier => carrier,
+        :Mode => mode,
+        :BillingCode => billing_code,
+        :Account => account
       },
-      :Notes => "This is a note",
+      :Notes => notes,
       :OrderLineItems => {
-        :OrderLineItem => [{
-          :Sku => "TestItem",
-          :Qty => "1"
-        }]
+        :OrderLineItem => [*prepare_products_body(products)]
       }
     }
+  end
+
+  def prepare_products_body(products)
+    products.map do |product|
+      {
+        :SKU => product[0].to_s,
+        :Qty => product[1].to_s
+      }
+    end
   end
 end
--- spec/lib/nutracoapi/create_order_spec.rb
+++ spec/lib/nutracoapi/create_order_spec.rb
@@ -2,8 +2,24 @@ require 'spec_helper'
 
 describe Nutracoapi::CreateOrder do
   it "create order call should work" do
-    response = subject.call_create_order(:reference_num => "Test123")
-    pp response.body
+    response = subject.call_create_order(
+      :reference_num => Time.now.to_i,
+      :company_name  => "Test Company Name",
+      :address1      => "Blablabla",
+      :city          => "Lost Angeles",
+      :state         => "Ca",
+      :zip           => "90010",
+      :country       => "US",
+      :carrier       => "UPS",
+      :mode          => "Ground",
+      :billing_code  => "FreightCollect",
+      :account       => "12345675",
+      :notes         => "This is a note",
+      :products      => {
+        :TestItem => 1
+      },
+    )
+    response.body[:int32].should == "1"
   end
 end
 
