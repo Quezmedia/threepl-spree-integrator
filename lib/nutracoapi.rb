4f772fd36ea21cfe03dcbdd84b473c3697d1dcd3
Filipe Chagas <filipe@ochagas.com>
2013-10-29 12:40:31 -0200

    Re include nutracoapi.rb without sensitive info

---
+++ lib/nutracoapi.rb
@@ -0,0 +1,25 @@
+require 'rails'
+require 'logger'
+require 'nutracoapi/engine'
+Dir["#{ File.expand_path(File.dirname(__FILE__)) }/nutracoapi/*.rb"].each{|f| require f}
+
+module Nutracoapi
+  mattr_accessor :login_parameters
+  @@login_parameters = {:ThreePLID => 111, :Login => "xxxxxxx", :Password => "xxxxxxx", :ThreePLKey =>"xxxxxxxxxxxxx", :FacilityID => "0", :CustomerID => "0"}
+
+  mattr_accessor :wsdl_endpoint
+  @@wsdl_endpoint = "https://app02.3plcentral.com/webserviceexternal/contracts.asmx?WSDL"
+
+  mattr_accessor :spree_endpoint
+  @@spree_endpoint = "http://example.com/api"
+
+  mattr_accessor :spree_token
+  @@spree_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+  # To permit Rails style config initalizer
+  class << self
+    def config
+      yield self
+    end
+  end
+end
