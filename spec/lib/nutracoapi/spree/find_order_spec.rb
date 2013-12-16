25eac919061e0b1a8e8c896f6c3e59a2bb0958c9
Filipe Chagas <filipe@ochagas.com>
2013-12-16 13:26:32 -0200

    Fixes broken tests

--- spec/lib/nutracoapi/spree/find_order_spec.rb
+++ spec/lib/nutracoapi/spree/find_order_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Nutracoapi::Spree::FindOrder do
   it "should return an order" do
-    result = subject.find_order("R582777812")
-    result.number.should == "R582777812"
+    result = subject.find_order("R131778586")
+    result.number.should == "R131778586"
   end
 end
--- spec/lib/nutracoapi/spree/ship_order_spec.rb
+++ spec/lib/nutracoapi/spree/ship_order_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 describe Nutracoapi::Spree::ShipOrder do
   it "should ship the order" do
-    result = subject.ship_order("R852711767", "H46403128353", "123")
-    result.number.should == "H46403128353"
+    result = subject.ship_order("R131778586", "H44833725578", "123")
+    result.number.should == "H44833725578"
     result.state.should == "shipped"
     result.tracking.should == "123"
   end
