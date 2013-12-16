e5377ff126c3395b09b3aff52cb43454d3e74c97
Filipe Chagas <filipe@ochagas.com>
2013-12-16 18:31:23 -0200

    Fix empty array problem

--- lib/nutracoapi/broker_three_to_spree.rb
+++ lib/nutracoapi/broker_three_to_spree.rb
@@ -5,7 +5,7 @@ class Nutracoapi::BrokerThreeToSpree
 
     non_shipped_orders.each do |order|
       three_order = three_find_order_provider.call_find_order(order.order_number)
-      if three_order
+      if three_order.try(:any?)
         tracking_number = three_order.first.tracking_number
         unless tracking_number.blank?
           spree_ship_provider.ship_order(order.order_number, order.shipment_number, tracking_number)
